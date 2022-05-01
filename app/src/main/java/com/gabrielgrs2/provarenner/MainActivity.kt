7bd4d67e80eca1116bf84511635b5dc72dc579fc
Gabriel Guilherme <gabriel_guilherme2006@hotmail.com>
2022-05-01 20:48:30 -0300

    Inital commit - configuring project :gear:

---
+++ app/src/main/java/com/gabrielgrs2/provarenner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gabrielgrs2.provarenner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
